a7f05c2c99436c00a1d74782c4af80a338fe02c3
Thiago Barros <thiagobarros95@gmail.com>
2024-01-31 09:07:45 -0300

    feat: add fluent setters and builder pattern

--- src/Domain/Entities/Registration.php
+++ src/Domain/Entities/Registration.php
@@ -16,53 +16,126 @@ final class Registration
     private DateTimeInterface $birthDate;
     private DateTimeInterface $registratedAt;
 
-    public function getName(): string
+    private function __construct()
     {
-        return $this->name;
     }
 
-    public function setName(string $name): void
+    /**
+     * Reset class instance
+     *
+     * @return  self
+     */
+    public static function reset(): static
     {
-        $this->name = $name;
+        return new Registration();
     }
-
-    public function getEmail(): string
-    {
-        return $this->email;
-    }
-
-    public function setEmail(Email $email): void
-    {
-        $this->email = $email;
-    }
-
-    public function getRegistrationNumber(): string
-    {
-        return $this->registrationNumber;
-    }
-
-    public function setRegistrationNumber(Cpf $registrationNumber): void
-    {
-        $this->registrationNumber = $registrationNumber;
-    }
-
-    public function getBirthDate(): DateTimeInterface
-    {
-        return $this->birthDate;
-    }
-
-    public function setBirthDate(DateTimeInterface $birthDate): void
-    {
-        $this->birthDate = $birthDate;
-    }
-
+    /**
+     * Get the value of registratedAt
+     */
     public function getRegistratedAt(): DateTimeInterface
     {
         return $this->registratedAt;
     }
 
-    public function setRegistratedAt(DateTimeInterface $registratedAt): void
+    /**
+     * Set the value of registratedAt
+     *
+     * @return  self
+     */
+    public function setRegistratedAt(DateTimeInterface $registratedAt): static
     {
         $this->registratedAt = $registratedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of birthDate
+     */
+    public function getBirthDate(): DateTimeInterface
+    {
+        return $this->birthDate;
+    }
+
+    /**
+     * Set the value of birthDate
+     *
+     * @return  self
+     */
+    public function setBirthDate(DateTimeInterface $birthDate): static
+    {
+        $this->birthDate = $birthDate;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of registrationNumber
+     */
+    public function getRegistrationNumber(): Cpf
+    {
+        return $this->registrationNumber;
+    }
+
+    /**
+     * Set the value of registrationNumber
+     *
+     * @return  self
+     */
+    public function setRegistrationNumber(Cpf $registrationNumber): static
+    {
+        $this->registrationNumber = $registrationNumber;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of email
+     */
+    public function getEmail(): Email
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @return  self
+     */
+    public function setEmail(Email $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of name
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Build class instance
+     *
+     * @return  self
+     */
+    public function build(): static
+    {
+        return $this;
     }
 }
