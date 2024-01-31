9b930fa71167e59622303f9127a7aea116157934
Thiago Barros <thiagobarros95@gmail.com>
2024-01-31 13:41:03 -0300

    test: add arch tests over App\UseCases\ExportRegistration\Dtos namespace

--- tests/Arch/ArchTest.php
+++ tests/Arch/ArchTest.php
@@ -1,5 +1,12 @@
 <?php
 
-Arch('src')
+Arch('App')
     ->expect('App')
     ->toUseStrictTypes();
+
+Arch('App\UseCases\ExportRegistration\Dtos')
+    ->expect('App\UseCases\ExportRegistration\Dtos')
+    ->toUseStrictTypes()
+    ->toBeClasses()
+    ->toBeReadonly()
+    ->not->toHaveSuffix('Dto');
