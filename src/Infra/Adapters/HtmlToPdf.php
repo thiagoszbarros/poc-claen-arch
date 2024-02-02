05c39f3ecb7cadb027c61460f25036ef9236d4d3
Thiago Barros <thiagobarros95@gmail.com>
2024-02-02 12:26:08 -0300

    feat: implement html to pdf adpter

---
+++ src/Infra/Adapters/HtmlToPdf.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infra\Adapters;
+
+use App\Domain\Entities\Registration;
+use App\Application\Contracts\ExportRegistrationPdfExporter;
+use Spipu\Html2Pdf\Html2Pdf;
+
+class HtmlToPdf implements ExportRegistrationPdfExporter
+{
+    public function export(Registration $registration): string
+    {
+        $html2pdf = new Html2Pdf('P', 'A4');
+
+        $html2pdf->setDefaultFont('Arial');
+        $html2pdf->writeHTML(
+            "<p>Nome: {$registration->getName()}</p><p>CPF: {$registration->getRegistrationNumber()}</p>"
+        );
+
+        return $html2pdf->output('', 'S');
+    }
+}
