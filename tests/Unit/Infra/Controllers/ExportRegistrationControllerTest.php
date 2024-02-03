f680973ae9ded0ae06d4bd9607e4810ccacc6049
Thiago Barros <thiagobarros95@gmail.com>
2024-02-03 01:23:32 -0300

    test: create test for export registration controller

---
+++ tests/Unit/Infra/Controllers/ExportRegistrationControllerTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
+use App\Infra\Adapters\HtmlToPdf;
+use App\Infra\Adapters\ServerStorage;
+use App\Infra\Presentation\ExportRegistrationPresenter;
+use App\Infra\Repositories\Stunt\StuntLoadRegistration;
+use App\Infra\Http\Controllers\ExportRegistrationController;
+use App\Application\UseCases\ExportRegistration\ExportRegistration;
+
+test('ExportRegistrationControllerTest', function () {
+
+    $req = new Request('GET', 'http://localhost', ['registrationNumber' => '06806573398',]);
+    $res = new Response();
+    $repo = new StuntLoadRegistration();
+    $pdfExporter = new HtmlToPdf();
+    $storage = new ServerStorage();
+    $useCase = new ExportRegistration($repo, $pdfExporter, $storage);
+   
+    $result = (new ExportRegistrationController($req, $res, $useCase))->execute(new ExportRegistrationPresenter());
+
+    expect(strval($result->getBody()))
+        ->toBeJson()
+        ->toBe(
+            '{"success":true,"errors":null,"message":"Registration exported sucessfully.","data":{"fullFileName":"registrations\/xpto.pdf"}}'
+        );
+});
